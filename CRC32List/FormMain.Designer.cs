2e7c505b470a0ebee7db72922fd4c77f180d234c
Vitaliy <mitradis@gmail.com>
2025-07-04 14:08:11 +0300

    Upload

---
+++ CRC32List/FormMain.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace CRC32List
+{
+    partial class FormMain
+    {
+        private System.ComponentModel.IContainer components = null;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.ColumnHeader CRC1;
+        private System.Windows.Forms.ColumnHeader CRC2;
+        private System.Windows.Forms.ColumnHeader filesName1;
+        private System.Windows.Forms.ColumnHeader filesName2;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ListView listView2;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog2;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.Timer timer1;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
+            this.button1 = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.CRC1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.filesName1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.listView2 = new System.Windows.Forms.ListView();
+            this.CRC2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.filesName2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.button2 = new System.Windows.Forms.Button();
+            this.openFileDialog2 = new System.Windows.Forms.OpenFileDialog();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.button3 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(253, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(60, 25);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Обзор";
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            this.openFileDialog1.Multiselect = true;
+            // 
+            // listView1
+            // 
+            this.listView1.AllowDrop = true;
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.CRC1,
+            this.filesName1});
+            this.listView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView1.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView1.FullRowSelect = true;
+            this.listView1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listView1.Location = new System.Drawing.Point(0, 0);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(334, 245);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView1_ColumnClick);
+            this.listView1.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView1_DragDrop);
+            this.listView1.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView1_DragEnter);
+            this.listView1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView1_KeyDown);
+            // 
+            // CRC1
+            // 
+            this.CRC1.Text = "CRC32:";
+            this.CRC1.Width = 85;
+            // 
+            // filesName1
+            // 
+            this.filesName1.Text = "Файлы:";
+            this.filesName1.Width = 227;
+            // 
+            // listView2
+            // 
+            this.listView2.AllowDrop = true;
+            this.listView2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.CRC2,
+            this.filesName2});
+            this.listView2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listView2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.listView2.FullRowSelect = true;
+            this.listView2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.listView2.Location = new System.Drawing.Point(0, 0);
+            this.listView2.Name = "listView2";
+            this.listView2.Size = new System.Drawing.Size(334, 132);
+            this.listView2.TabIndex = 3;
+            this.listView2.UseCompatibleStateImageBehavior = false;
+            this.listView2.View = System.Windows.Forms.View.Details;
+            this.listView2.ColumnClick += new System.Windows.Forms.ColumnClickEventHandler(this.listView2_ColumnClick);
+            this.listView2.DragDrop += new System.Windows.Forms.DragEventHandler(this.listView2_DragDrop);
+            this.listView2.DragEnter += new System.Windows.Forms.DragEventHandler(this.listView2_DragEnter);
+            this.listView2.KeyDown += new System.Windows.Forms.KeyEventHandler(this.listView2_KeyDown);
+            // 
+            // CRC2
+            // 
+            this.CRC2.Text = "CRC32:";
+            this.CRC2.Width = 85;
+            // 
+            // filesName2
+            // 
+            this.filesName2.Text = "Файлы:";
+            this.filesName2.Width = 227;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(253, 1);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(60, 25);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Обзор";
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // openFileDialog2
+            // 
+            this.openFileDialog2.FileName = "openFileDialog2";
+            this.openFileDialog2.Multiselect = true;
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer1.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.button3);
+            this.splitContainer1.Panel1.Controls.Add(this.button1);
+            this.splitContainer1.Panel1.Controls.Add(this.listView1);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.button2);
+            this.splitContainer1.Panel2.Controls.Add(this.listView2);
+            this.splitContainer1.Size = new System.Drawing.Size(334, 381);
+            this.splitContainer1.SplitterDistance = 245;
+            this.splitContainer1.TabIndex = 0;
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Arial", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.ForeColor = System.Drawing.Color.Gray;
+            this.button3.Location = new System.Drawing.Point(187, 1);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(60, 25);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "MD5";
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerReportsProgress = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            this.backgroundWorker2.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker2_ProgressChanged);
+            this.backgroundWorker2.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker2_RunWorkerCompleted);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // FormMain
+            // 
+            this.ClientSize = new System.Drawing.Size(334, 381);
+            this.Controls.Add(this.splitContainer1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(350, 999999);
+            this.MinimumSize = new System.Drawing.Size(350, 420);
+            this.Name = "FormMain";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "CRC32 List";
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+    }
+}
+
---
+++ CRC32List/FormMain.cs
@@ -0,0 +1,441 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+
+namespace CRC32List
+{
+    public partial class FormMain : Form
+    {
+        List<string> dropList1 = new List<string>();
+        List<string> dropList2 = new List<string>();
+        string lastPath1 = null;
+        string lastPath2 = null;
+        bool selectList = true;
+        bool folder = false;
+        bool md5 = false;
+
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        void button1_Click(object sender, EventArgs e)
+        {
+            addFiles(true);
+        }
+
+        void button2_Click(object sender, EventArgs e)
+        {
+            addFiles(false);
+        }
+
+        void button3_Click(object sender, EventArgs e)
+        {
+            md5 = !md5;
+            if (!backgroundWorker1.IsBusy && !backgroundWorker2.IsBusy)
+            {
+                Text = md5 ? "MD5 List" : "CRC32 List";
+            }
+            button3.ForeColor = md5 ? Color.Firebrick : Color.Gray;
+        }
+
+        void addFiles(bool button)
+        {
+            if ((button ? lastPath1 : lastPath2) != null)
+            {
+                (button ? openFileDialog1 : openFileDialog2).InitialDirectory = button ? lastPath1 : lastPath2;
+            }
+            DialogResult result = button ? openFileDialog1.ShowDialog() : openFileDialog2.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                if (button)
+                {
+                    lastPath1 = Path.GetDirectoryName(openFileDialog1.FileNames[0]);
+                }
+                else
+                {
+                    lastPath2 = Path.GetDirectoryName(openFileDialog2.FileNames[0]);
+                }
+                (button ? dropList1 : dropList2).AddRange(button ? openFileDialog1.FileNames : openFileDialog2.FileNames);
+                prepareState(button);
+            }
+        }
+
+        void listView1_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) && !backgroundWorker1.IsBusy)
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        void listView2_DragEnter(object sender, DragEventArgs e)
+        {
+            if (e.Data.GetDataPresent(DataFormats.FileDrop, false) && !backgroundWorker2.IsBusy)
+            {
+                e.Effect = DragDropEffects.All;
+            }
+        }
+
+        void listView1_DragDrop(object sender, DragEventArgs e)
+        {
+            dropList1.AddRange((string[])e.Data.GetData(DataFormats.FileDrop));
+            prepareState(true);
+        }
+
+        void listView2_DragDrop(object sender, DragEventArgs e)
+        {
+            dropList2.AddRange((string[])e.Data.GetData(DataFormats.FileDrop));
+            prepareState(false);
+        }
+
+        void prepareState(bool list)
+        {
+            (list ? button1 : button2).Enabled = false;
+            (list ? CRC1 : CRC2).Text = md5 ? "MD5" : "CRC32";
+            (list ? dropList1 : dropList2).Sort();
+            (list ? dropList1 : dropList2).Add(list ? "listView1" : "listView2");
+            (list ? listView1 : listView2).Items.Clear();
+            foreach (ListViewItem item in (list ? listView2 : listView1).Items)
+            {
+                item.BackColor = SystemColors.Window;
+            }
+            (list ? backgroundWorker1 : backgroundWorker2).RunWorkerAsync(list ? dropList1 : dropList2);
+        }
+
+        void backgroundWorker1_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            e.Result = getItems((List<string>)e.Argument, worker);
+        }
+
+        void backgroundWorker2_DoWork(object sender, System.ComponentModel.DoWorkEventArgs e)
+        {
+            BackgroundWorker worker = sender as BackgroundWorker;
+            e.Result = getItems((List<string>)e.Argument, worker);
+        }
+
+        List<ListViewItem> getItems(List<string> list, BackgroundWorker worker)
+        {
+            List<ListViewItem> outList = new List<ListViewItem>();
+            int count = list.Count - 1;
+            outList.Add(new ListViewItem(list[count]));
+            for (int i = 0; i < count; i++)
+            {
+                if (File.Exists(list[i]))
+                {
+                    worker.ReportProgress(i);
+                    ListViewItem item = new ListViewItem(new string[] { md5 ? getMD5(list[i]) : getCRC(list[i]), Path.GetFileName(list[i]), list[i] });
+                    outList.Add(item);
+                }
+            }
+            return outList;
+        }
+
+        void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressState(true, e.ProgressPercentage);
+        }
+
+        void backgroundWorker2_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressState(false, e.ProgressPercentage);
+        }
+
+        void progressState(bool list, int index)
+        {
+            Text = (md5 ? "MD5 List: " : "CRC32 List: ") + Path.GetFileName((list ? dropList1 : dropList2)[index]);
+        }
+
+        void backgroundWorker1_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            completedState((List<ListViewItem>)e.Result);
+        }
+
+        void backgroundWorker2_RunWorkerCompleted(object sender, System.ComponentModel.RunWorkerCompletedEventArgs e)
+        {
+            completedState((List<ListViewItem>)e.Result);
+        }
+
+        void completedState(List<ListViewItem> result)
+        {
+            bool list = result[0].Text == "listView1";
+            result.RemoveAt(0);
+            (list ? listView1 : listView2).Items.AddRange(result.ToArray());
+            if (listView1.Items.Count > 0 && listView2.Items.Count > 0)
+            {
+                compareLists();
+            }
+            (list ? filesName1 : filesName2).Text = (list ? listView1 : listView2).Items.Count > 0 ? "Файлы" + " (" + (list ? listView1 : listView2).Items.Count + "):" : "Файлы";
+            if (list ? !backgroundWorker2.IsBusy : !backgroundWorker1.IsBusy)
+            {
+                Text = md5 ? "MD5 List" : "CRC32 List";
+            }
+            (list ? dropList1 : dropList2).Clear();
+            (list ? button1 : button2).Enabled = true;
+        }
+
+        void compareLists()
+        {
+            int count1 = listView1.Items.Count;
+            int count2 = listView2.Items.Count;
+            for (int i = 0; i < count1; i++)
+            {
+                for (int j = 0; j < count2; j++)
+                {
+                    if (String.Equals(listView1.Items[i].Text, listView2.Items[j].Text, StringComparison.OrdinalIgnoreCase))
+                    {
+                        if (String.Equals(listView1.Items[i].SubItems[1].Text, listView2.Items[j].SubItems[1].Text, StringComparison.OrdinalIgnoreCase))
+                        {
+                            listView1.Items[i].BackColor = Color.PaleGreen;
+                            listView2.Items[j].BackColor = Color.PaleGreen;
+                        }
+                        else
+                        {
+                            if (listView1.Items[i].BackColor == SystemColors.Window)
+                            {
+                                listView1.Items[i].BackColor = Color.LemonChiffon;
+                            }
+                            if (listView2.Items[j].BackColor == SystemColors.Window)
+                            {
+                                listView2.Items[j].BackColor = Color.LemonChiffon;
+                            }
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < count1; i++)
+            {
+                if (listView1.Items[i].BackColor == SystemColors.Window)
+                {
+                    for (int j = 0; j < count2; j++)
+                    {
+                        if (listView2.Items[j].BackColor == SystemColors.Window && String.Equals(listView1.Items[i].SubItems[1].Text, listView2.Items[j].SubItems[1].Text, StringComparison.OrdinalIgnoreCase))
+                        {
+                            listView1.Items[i].BackColor = Color.MistyRose;
+                            listView2.Items[j].BackColor = Color.MistyRose;
+                        }
+                    }
+                }
+            }
+        }
+
+        void listView1_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            reSortColums(true, e.Column);
+        }
+
+        void listView2_ColumnClick(object sender, ColumnClickEventArgs e)
+        {
+            reSortColums(false, e.Column);
+        }
+
+        void reSortColums(bool list, int column)
+        {
+            if ((list ? listView1 : listView2).Sorting == SortOrder.Ascending)
+            {
+                (list ? listView1 : listView2).Sorting = SortOrder.Descending;
+            }
+            else
+            {
+                (list ? listView1 : listView2).Sorting = SortOrder.Ascending;
+            }
+            (list ? listView1 : listView2).Sort();
+            (list ? listView1 : listView2).ListViewItemSorter = new ListViewItemComparer(column, (list ? listView1 : listView2).Sorting);
+        }
+
+        void listView1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D1)
+            {
+                timer1.Enabled = true;
+                selectList = true;
+                folder = false;
+            }
+            if (e.KeyCode == Keys.D2)
+            {
+                timer1.Enabled = true;
+                selectList = true;
+                folder = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.C)
+            {
+                toMemory(true);
+            }
+            e.SuppressKeyPress = true;
+        }
+
+        void listView2_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.D1)
+            {
+                timer1.Enabled = true;
+                selectList = false;
+                folder = false;
+            }
+            if (e.KeyCode == Keys.D2)
+            {
+                timer1.Enabled = true;
+                selectList = false;
+                folder = true;
+            }
+            else if (e.Control && e.KeyCode == Keys.C)
+            {
+                toMemory(false);
+            }
+            e.SuppressKeyPress = true;
+        }
+
+        void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Enabled = false;
+            runFile(selectList);
+        }
+
+        void runFile(bool list)
+        {
+            if ((list ? listView1 : listView2).SelectedItems.Count > 0)
+            {
+                string target = (list ? listView1 : listView2).SelectedItems[0].SubItems[2].Text;
+                if (folder)
+                {
+                    target = Path.GetDirectoryName(target);
+                }
+                if (folder ? Directory.Exists(target) : File.Exists(target))
+                {
+                    try
+                    {
+                        Process process = new Process();
+                        process.StartInfo.FileName = target;
+                        process.StartInfo.WorkingDirectory = Path.GetDirectoryName(target);
+                        process.Start();
+                        process = null;
+                    }
+                    catch
+                    {
+                        MessageBox.Show("Не удалось открыть: " + target);
+                    }
+                }
+            }
+        }
+
+        void toMemory(bool list)
+        {
+            if ((list ? listView1 : listView2).SelectedItems.Count > 0)
+            {
+                List<string> result = new List<string>();
+                int count = (list ? listView1 : listView2).SelectedItems.Count;
+                for (int i = 0; i < count; i++)
+                {
+                    result.Add((list ? listView1 : listView2).SelectedItems[i].Text + "\t" + (list ? listView1 : listView2).SelectedItems[i].SubItems[1].Text);
+                }
+                Clipboard.SetText(String.Join(Environment.NewLine, result));
+                result.Clear();
+            }
+        }
+
+        string getMD5(string file)
+        {
+            MD5 md5 = MD5.Create();
+            Stream st = File.OpenRead(file);
+            byte[] hash = md5.ComputeHash(st);
+            md5.Clear();
+            st.Close();
+            return BitConverter.ToString(hash).Replace("-", "").ToUpperInvariant();
+
+        }
+
+        string getCRC(string file)
+        {
+            string line = "";
+            try
+            {
+                FileStream streamFile = File.OpenRead(file);
+                line = String.Format("{0:X}", calculateCRC(streamFile));
+                streamFile.Close();
+            }
+            catch
+            {
+                MessageBox.Show("Нет доступа к: " + file);
+            }
+            while (line.Length != 8)
+            {
+                line = "0" + line;
+            }
+            return line;
+        }
+
+        uint calculateCRC(Stream stream)
+        {
+            const int buffer_size = 1024;
+            const uint POLYNOMIAL = 0xEDB88320;
+            uint result = 0xFFFFFFFF;
+            uint Crc32;
+            byte[] buffer = new byte[buffer_size];
+            uint[] table = new uint[256];
+            unchecked
+            {
+                for (int i = 0; i < 256; i++)
+                {
+                    Crc32 = (uint)i;
+                    for (int j = 8; j > 0; j--)
+                    {
+                        if ((Crc32 & 1) == 1)
+                        {
+                            Crc32 = (Crc32 >> 1) ^ POLYNOMIAL;
+                        }
+                        else
+                        {
+                            Crc32 >>= 1;
+                        }
+                    }
+                    table[i] = Crc32;
+                }
+                int count = stream.Read(buffer, 0, buffer_size);
+                while (count > 0)
+                {
+                    for (int i = 0; i < count; i++)
+                    {
+                        result = ((result) >> 8) ^ table[(buffer[i]) ^ ((result) & 0x000000FF)];
+                    }
+                    count = stream.Read(buffer, 0, buffer_size);
+                }
+            }
+            buffer = null;
+            table = null;
+            stream.Close();
+            return ~result;
+        }
+    }
+
+    class ListViewItemComparer : IComparer
+    {
+        SortOrder order;
+        int col;
+        ListViewItemComparer()
+        {
+            col = 0;
+            order = SortOrder.Ascending;
+        }
+        public ListViewItemComparer(int column, SortOrder order)
+        {
+            col = column;
+            this.order = order;
+        }
+        public int Compare(object x, object y)
+        {
+            int returnVal = String.Compare(((ListViewItem)x).SubItems[col].Text, ((ListViewItem)y).SubItems[col].Text);
+            if (order == SortOrder.Descending)
+            {
+                returnVal *= -1;
+            }
+            return returnVal;
+        }
+    }
+}
